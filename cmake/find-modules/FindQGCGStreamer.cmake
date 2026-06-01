7e5e0b7f09e813b2d4547433c8739d99bc5ff779
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2026-06-01 11:28:45 +0100

    Fix rswebrtc bundle on Android and Linux

--- cmake/find-modules/FindQGCGStreamer.cmake
+++ cmake/find-modules/FindQGCGStreamer.cmake
@@ -812,7 +812,7 @@ if(NOT DEFINED GSTREAMER_PLUGINS)
     )
     # Deferred for all platforms — GStreamer_VERSION is populated after find_package(GStreamer) below.
     if(ANDROID)
-        list(APPEND GSTREAMER_PLUGINS androidmedia dav1d)
+        list(APPEND GSTREAMER_PLUGINS androidmedia)
     elseif(APPLE)
         list(APPEND GSTREAMER_PLUGINS applemedia dav1d vulkan)
     elseif(WIN32)
